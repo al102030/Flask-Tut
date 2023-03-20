f5484a6b652ba8afc77aca1dafe434e00480f08b
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-03-20 15:12:45 +0330

    Add Bootsrap style an basic Html file.

--- file1.py
+++ file1.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello! this is the main page. <h1>HELLO</h1>"
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html")
+
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("user", name='Admin!'))
+
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True)
