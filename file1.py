8569117fe51c431acf6c56153e68d04ac711a37d
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-03-21 13:56:12 +0330

    Add GET and POST methods and add session data.

--- file1.py
+++ file1.py
@@ -1,22 +1,35 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
 
 app = Flask(__name__)
+app.secret_key = "aklsdd"
 
 
-@app.route("/<name>")
-def home(name):
+@app.route("/")
+def home():
     return render_template("index.html")
 
 
-@app.route('/<name>')
-def user(name):
-    return f"Hello {name}!"
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        u = request.form["nm"]
+        session["user"] = u
+        return redirect(url_for("user"))
+    return render_template("login.html")
 
 
-@app.route('/admin')
-def admin():
-    return redirect(url_for("user", name='Admin!'))
+@app.route("/user")
+def user():
+    if "user" in session:
+        u = session["user"]
+        return f"<h1>{u}</h1>"
+    return redirect(url_for("login"))
+
+
+# @app.route('/admin')
+# def admin():
+#     return redirect(url_for("user", name='Admin!'))
 
 
 if __name__ == "__main__":
