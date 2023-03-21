dcb24b6aced79faa755be37e760c0d339eac5b62
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-03-21 23:02:12 +0330

    Session methods added.

--- file1.py
+++ file1.py
@@ -1,8 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 
 app = Flask(__name__)
 app.secret_key = "aklsdd"
+app.permanent_session_lifetime = timedelta(days=2)
 
 
 @app.route("/")
@@ -13,8 +15,11 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         u = request.form["nm"]
         session["user"] = u
+        return redirect(url_for("user"))
+    if "user" in session:
         return redirect(url_for("user"))
     return render_template("login.html")
 
@@ -27,9 +32,10 @@ def user():
     return redirect(url_for("login"))
 
 
-# @app.route('/admin')
-# def admin():
-#     return redirect(url_for("user", name='Admin!'))
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
